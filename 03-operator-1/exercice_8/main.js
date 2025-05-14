43911abd461ac1977897a4cfd88bbc6ca4d4ff70
Dieudonné Dipa <dieudonne.dipa@gmail.com>
2025-05-14 15:00:58 +0100

    Update main.js

--- 03-operator-1/exercice_8/main.js
+++ 03-operator-1/exercice_8/main.js
@@ -1,11 +1,21 @@
-let salaireMensuel,
+/*let salaireMensuel,
   loyer,
   nourriture,
   transport,
   autresDepenses,
   totalDepenses,
-  reste;
+  reste;*/
 // Ecrivez votre code ici
+
+let salaireMensuel = 500;
+
+let loyer = salaireMensuel * 0.30;
+let nourriture = salaireMensuel * 0.20;
+let transport = salaireMensuel * 0.10;
+let autresDepenses = 50 + 25;
+
+let totalDepenses = loyer + nourriture + transport + autresDepenses;
+let reste = salaireMensuel - totalDepenses;
 
 // Ne pas modifier le code ci-dessous
 module.exports = {
