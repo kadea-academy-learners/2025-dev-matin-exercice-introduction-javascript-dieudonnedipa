2bcf1ccc2be12ff47c58df3309f8dca45e53b0d0
Dieudonné Dipa <dieudonne.dipa@gmail.com>
2025-05-14 15:00:08 +0100

    Update main.js

--- 03-operator-1/exercice_7/main.js
+++ 03-operator-1/exercice_7/main.js
@@ -1,5 +1,11 @@
-let ageEnfant, agePere, ageMere, ageGrandPere, ageOncle;
+/*let ageEnfant, agePere, ageMere, ageGrandPere, ageOncle;*/
 // Ecrivez votre code ici
+
+let ageEnfant = 10;
+let agePère = ageEnfant * 2;
+let ageMère = agePère - 5;
+let ageGrandPère = (ageMère * 2) + (ageEnfant / 2);
+let ageOncle = agePère + 10;
 
 // Ne pas modifier le code ci-dessous
 module.exports = {
