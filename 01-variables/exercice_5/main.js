440582a14744ad7bd1218b5d89f98b7b91cfb0d1
Dieudonné Dipa <dieudonne.dipa@gmail.com>
2025-05-14 14:55:26 +0100

    Update main.js

--- 01-variables/exercice_5/main.js
+++ 01-variables/exercice_5/main.js
@@ -1,4 +1,4 @@
-// ecrire le code de conversion ici
+/*// ecrire le code de conversion ici
 let celsius;
 
 // Conversion Celsius à Fahrenheit
@@ -7,7 +7,11 @@ function celsiusToFahrenheit(celsius) {
   let fahrenheit;
   // la ligne suivante ne doit pas être modifiée
   return fahrenheit;
-}
+}*/
+
+let celsius = 25;
+let fahrenheit = (celsius * 9/5) + 32;
+console.log(fahrenheit);
 
 // les lignes suivantes ne sont pas à modifier
 module.exports = { celsiusToFahrenheit };
