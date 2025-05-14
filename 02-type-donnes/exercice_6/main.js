545c559e25b5107b713bb40b6fcd818d57ce3eaa
Dieudonné Dipa <dieudonne.dipa@gmail.com>
2025-05-14 14:56:15 +0100

    Update main.js

--- 02-type-donnes/exercice_6/main.js
+++ 02-type-donnes/exercice_6/main.js
@@ -1,7 +1,13 @@
-let age;
+/*let age;
 let nom;
 let estConnecte;
 let valeurNulle;
+let inconnue;*/
+
+let age = 30;
+let nom = "Alice";
+let estConnecte = true;
+let valeurNulle = null;
 let inconnue;
 
 // Les lignes ne doivent pas être modifiées
